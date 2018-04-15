ea28113515dd7a407f51614d9a36c30fdc1d87e8
Graham Long <longevity_software@mail.com>
2018-04-15 23:05:05 +0100

    added some response codes to database_requests.

--- database_requests.php
+++ database_requests.php
@@ -11,15 +11,25 @@ if(isset($_POST['action']))
     switch($_POST['action'])
     {
         case 'add':
+            // return an accepted response code
+            http_response_code(202);
             add_item_to_database($_POST['checked'],$_POST['description'],$_POST['quantity'],$_POST['price']);
             break;
         case 'delete':
+            // return an accepted response code
+            http_response_code(202);
             delete_item_from_database_using_description($_POST['description']);
             break;
         default:
-            
+            // action not recognised so return a bad request response code
+            http_response_code(400);
             break;
     }
+}
+else
+{
+    // return a method not allowed response code
+    http_response_code(405);
 }
 
 ?>
